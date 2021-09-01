91122524fa38bb6e69628a86c30f60ce13dffa87
AdamDriverJr <72190682+AdamDriverJr@users.noreply.github.com>
2021-09-01 12:06:38 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<h1>
+          Stay Connected
+        </h1>
+        <p>
+          Receive weekly insights from industry insiders.
+        </p>
+        <a class="btn" href="">Join</a>
+      </div>
+    </div>
+    <div class="footer">
+      <p>
+        @Innovation Cloud Conference
+      </p>
+    </div>
+  </body>
+</html>
